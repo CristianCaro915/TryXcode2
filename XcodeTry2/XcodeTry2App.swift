c8c9df990c0ec159ab1272b312daf83fae248a97
CristianCaro915 <c.caro@uniandes.edu.co>
2024-09-04 22:26:01 -0500

    Initial commit

---
+++ XcodeTry2/XcodeTry2App.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTry2App.swift
+//  XcodeTry2
+//
+//  Created by Cristian Caro on 4/09/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTry2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeTry2UITests/XcodeTry2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  XcodeTry2UITestsLaunchTests.swift
+//  XcodeTry2UITests
+//
+//  Created by Cristian Caro on 4/09/24.
+//
+
+import XCTest
+
+final class XcodeTry2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
